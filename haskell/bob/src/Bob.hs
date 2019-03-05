ee5079f6c0dda35f37ea7e719f9a333129db49f8
Alberto Nicoletti <albyx.n@gmail.com>
2019-03-05 19:42:00 +0100

    Haskell Bob iteration 2

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -3,19 +3,27 @@ module Bob (responseFor) where
 import Data.Char (isUpper, isSpace, isLetter)
 import Data.List (dropWhileEnd)
 
-isShouting :: String -> Bool
-isShouting xs = all (\x -> isUpper x || not (isLetter x)) xs && any isUpper xs
-
 trim :: String -> String
 trim xs = dropWhile isSpace (dropWhileEnd isSpace xs)
 
+isShouting :: String -> Bool
+isShouting xs = all (\x -> isUpper x || not (isLetter x)) xs && any isUpper xs
+
+isQuestion :: String -> Bool
+isQuestion xs = last xs == '?'
+
+isShoutingAQuestion :: String -> Bool
+isShoutingAQuestion xs = isShouting xs && isQuestion xs
+
+isOnlyWhitespace :: String -> Bool
+isOnlyWhitespace xs = all isSpace xs
+
 responseFor :: String -> String
+responseFor [] = "Fine. Be that way!"
 responseFor xs
-    | null trimmedXs = "Fine. Be that way!"
-    | isShouting trimmedXs && last trimmedXs == '!' = "Whoa, chill out!"
-    | isShouting trimmedXs && last trimmedXs == '?' = "Calm down, I know what I'm doing!"
+    | isOnlyWhitespace xs = "Fine. Be that way!"
+    | isShoutingAQuestion trimmedXs = "Calm down, I know what I'm doing!"
     | isShouting trimmedXs = "Whoa, chill out!"
-    | last trimmedXs == '?' = "Sure."
-    | all isSpace trimmedXs = "Fine. Be that way!"
+    | isQuestion trimmedXs = "Sure."
     | otherwise = "Whatever."
     where trimmedXs = trim xs
